e008451f42a7030ecd2b78fd7c6288fcc3073b46
crisog <ortega.cpp@gmail.com>
2021-09-11 02:28:29 -0400

    feat: add discovery content view

---
+++ MenuApp/Views/DiscoveryButtonView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MenuApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DiscoveryButtonView : ContentView
+    {
+        public DiscoveryButtonView()
+        {
+            InitializeComponent();
+        }
+    }
+}
