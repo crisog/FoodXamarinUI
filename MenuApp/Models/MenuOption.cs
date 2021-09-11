eedcfb535dcb61ec88119c3e480779db69892167
crisog <ortega.cpp@gmail.com>
2021-09-11 02:27:23 -0400

    Add models

---
+++ MenuApp/Models/MenuOption.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MenuApp.Models
+{
+    public class MenuOption
+    {
+        public string BackgroundColor { get; set; }
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+    }
+}
