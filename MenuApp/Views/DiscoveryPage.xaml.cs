b9b1f709ab833fda7b358eac5fd3af75df398295
crisog <ortega.cpp@gmail.com>
2021-09-11 02:28:41 -0400

    feat: add discovery page

---
+++ MenuApp/Views/DiscoveryPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using MenuApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MenuApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DiscoveryPage : ContentPage
+    {
+        DiscoveryButtonView lastElementSelected = new DiscoveryButtonView();
+        public DiscoveryPage()
+        {
+            InitializeComponent();
+            BindingContext = new DiscoveryPageViewModel();
+        }
+
+        public void Stack_Clicked(object sender, EventArgs e)
+        {
+            lastElementSelected.BackgroundColor = Color.White;
+            var elementSelected = (DiscoveryButtonView)sender;
+            elementSelected.BackgroundColor = Color.FromHex("#F9A602");
+            lastElementSelected = elementSelected;
+        }
+    }
+}
